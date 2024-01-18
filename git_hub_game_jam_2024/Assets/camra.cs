e88304711118c375ce5835d73397edb1b73191c9
ViCeshogun <156317435+ViCeshogun@users.noreply.github.com>
2024-01-18 15:51:50 +0100

    last leval

--- git_hub_game_jam_2024/Assets/camra.cs
+++ git_hub_game_jam_2024/Assets/camra.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Diagnostics.SymbolStore;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
@@ -15,10 +16,12 @@ public class camra : MonoBehaviour
     public int time;
     public Transform bullet_spin;
     public float random_check;
-    
+    public GameObject camra_;
 
     public void Update()
     {
+
+
         float number = Mathf.Atan2(bob_pos.y - player_pos.y, bob_pos.x - player_pos.x);
         if (time == 1) {
             player_pos = player.transform.position;
@@ -39,9 +42,7 @@ public class camra : MonoBehaviour
 
         if (time > 4&& time<6) 
         {
-            bullet.transform.position = Vector3.MoveTowards(bullet.transform.position, bob_pos, 0.03f);
-
-
+            bullet.transform.position = Vector3.MoveTowards(bullet.transform.position, bob_pos, 0.05f);
         }
         
         
@@ -50,11 +51,11 @@ public class camra : MonoBehaviour
         if (time == 6) { 
             time = 0; 
             random_check = 0; }
-        if (random == 1) { bob_pos = new Vector2(-11.5f, 0); }
-        if (random == 2) { bob_pos = new Vector2(11.5f, 0); }
-        if (random == 3) { bob_pos = new Vector2(0, 5.5f); }
-        if (random == 4) { bob_pos = new Vector2(11.5f, 5.5f); }
-        if (random == 5) { bob_pos = new Vector2(-11.5f, 5.5f); }
+        if (random == 1) { bob_pos = new Vector2(-11.5f+camra_.transform.position.x, 0+ camra_.transform.position.y); }
+        if (random == 2) { bob_pos = new Vector2(11.5f + camra_.transform.position.x, 0 + camra_.transform.position.y); }
+        if (random == 3) { bob_pos = new Vector2(0 + camra_.transform.position.x, 5.5f + camra_.transform.position.y); }
+        if (random == 4) { bob_pos = new Vector2(11.5f + camra_.transform.position.x, 5.5f + camra_.transform.position.y); }
+        if (random == 5) { bob_pos = new Vector2(-11.5f + camra_.transform.position.x, 5.5f + camra_.transform.position.y); }
         if (random_check == 0) { random = Random.Range(1, 6); random_check = 1; }
 
 
@@ -70,13 +71,23 @@ public class camra : MonoBehaviour
         if (random == 3) { bob_pos = new Vector2(0, 5.5f); random = 0; bullet.transform.position = bob_pos; }
         if (random == 4) { bob_pos = new Vector2(11.5f, 5.5f); random = 0; bullet.transform.position = bob_pos; }
         if (random == 5) { bob_pos = new Vector2(-11.5f, 5.5f); random = 0; bullet.transform.position = bob_pos; }
-    }
+    
 
-    IEnumerator time_randomiser() 
+       
+        
+      
+    }
+    
+
+
+
+
+        IEnumerator time_randomiser() 
     {
         yield return new WaitForSeconds(1);
         time = time + 1;
         StartCoroutine(time_randomiser());
 
     }
+  
 }
