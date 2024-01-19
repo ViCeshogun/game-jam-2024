cbe6b34c1f1559cf0db2de25360901721096858e
CoolUsername28 <felix.rahm@elev.ga.lbs.se>
2024-01-19 10:18:08 +0100

    Timerscript + level1

--- git_hub_game_jam_2024/Assets/timer.cs
+++ git_hub_game_jam_2024/Assets/timer.cs
@@ -1,11 +1,12 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class timer : MonoBehaviour
 {
     // the value that will show up
     public float time;
+    [SerializeField] private Text TimerText;
 
     // Start is called before the first frame update
     public void Start()
@@ -23,9 +24,10 @@ public class timer : MonoBehaviour
     public IEnumerator Timer()
     {
         // the time between the change in seconds 
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(1f);
         // the values change
-        time += 0.1f;
+        time += 1f;
+        TimerText.text = "Time: " + time;
         // the continuation of the timer
         StartCoroutine(Timer());
 
